1bc02fff5bf09466eee10f6ce6a9cde53ae64a7f
Carrie Li <carrie.t.li@gmail.com>
2018-11-09 00:41:19 +0000

    Converted methods to @staticmethod

--- cust-output.py
+++ cust-output.py
@@ -13,6 +13,8 @@
     f. Column 3: Address (free text)
     g. Column 4: Zip Code. Five-digit format; pad with zeroes if < 5
     h. Column 5: Consumption (numeric value)
+
+    (No dependencies required.)
 """
 
 import csv
@@ -21,14 +23,16 @@ from datetime import datetime
 
 class CustomerData:
 
+    # Class attributes
     fieldnames = [  'Account Number',
                     'Read Date',
                     'Address',
                     'Zip Code',
                     'Consumption'  ]
 
+
     def transform_data(self):
-        """ Reads the input file and returns the data in a list of items.
+        """ Reads the input file and returns the data as a list of items.
         """
 
         seen = set() # To check for duplicates
@@ -66,47 +70,49 @@ class CustomerData:
         return
 
 
-    def pad_zeroes(self, string, digits):
+    @staticmethod
+    def pad_zeroes(string, digits):
         """ Takes string and pads it with leading zeroes.
 
             Test:
 
-            >>> cust = CustomerData()
-            >>> cust.pad_zeroes('45', 6)
+            >>> CustomerData.pad_zeroes('45', 6)
             '000045'
 
-            >>> cust.pad_zeroes('1003', 5)
+            >>> CustomerData.pad_zeroes('1003', 5)
             '01003'
         """
 
         return str(int(string)).zfill(digits)
 
-    def convert_to_int(self, string):
+
+    @staticmethod
+    def convert_to_int(string):
         """ Takes a string of a positive or negative number and converts it to
             an integer.
 
             Test:
-            >>> cust = CustomerData()
-            >>> cust.convert_to_int('1,232')
+            >>> CustomerData.convert_to_int('1,232')
             1232
 
-            >>> cust.convert_to_int('(121)')
+            >>> CustomerData.convert_to_int('(121)')
             -121
         """
 
         return int(string.replace(',', '').replace(')', '').replace('(', '-'))
 
-    def convert_date(self, string):
+
+    @staticmethod
+    def convert_date(string):
         """ Takes a string and converts it to a datetime object, then transforms
             the date format to YYYYMMDD.
 
             Test:
 
-            >>> cust = CustomerData()
-            >>> cust.convert_date('Feb 25 2017')
+            >>> CustomerData.convert_date('Feb 25 2017')
             '20170225'
 
-            >>> cust.convert_date('Sep 2 2016 ')
+            >>> CustomerData.convert_date('Sep 2 2016 ')
             '20160902'
         """
 
